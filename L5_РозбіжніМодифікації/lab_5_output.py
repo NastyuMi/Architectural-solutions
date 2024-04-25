17aced1460e73eeedd3e7f31337a741ce732118e
NastyuMi <90446202+NastyuMi@users.noreply.github.com>
2024-04-26 01:28:26 +0300

    lab5

---
+++ L5_РозбіжніМодифікації/lab_5_output.py
@@ -0,0 +1,25 @@
+# output_1.py
+
+class Product:
+    def __init__(self, name, price, type):
+        self.name = name
+        self.price = price
+        self.type = type
+
+class ProductSearch:
+    @staticmethod
+    def search_product(products, query):
+        # Пошук товару за запитом
+        pass
+
+class ProductDisplay:
+    @staticmethod
+    def display_product(product):
+        # Відображення інформації про товар
+        pass
+
+class ProductOrder:
+    @staticmethod
+    def order_product(product, quantity):
+        # Замовлення товару
+        pass
