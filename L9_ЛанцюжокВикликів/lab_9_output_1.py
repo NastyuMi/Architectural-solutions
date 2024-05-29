e17146cf4320180bac8a9af58d64cd616df1e5de
NastyuMi <90446202+NastyuMi@users.noreply.github.com>
2024-05-30 00:35:49 +0300

    lab 9

---
+++ L9_ЛанцюжокВикликів/lab_9_output_1.py
@@ -0,0 +1,25 @@
+class Customer:
+    def __init__(self, account):
+        self.account = account
+
+    def get_account(self):
+        return self.account
+
+class Account:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def get_balance(self):
+        return self.balance
+
+class Bank:
+    def __init__(self, customer):
+        self.customer = customer
+
+    def get_balance(self):
+        return self.customer.get_account().get_balance()
+
+# Користування новим методом для отримання балансу
+bank = Bank(Customer(Account(1000)))
+balance = bank.get_balance()
+print(f"The balance is: {balance}")
